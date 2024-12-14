aa31411d371320858549c95e7b96c69bac77b07a
yuk1-ishida <lionact08@gmail.com>
2024-12-14 12:57:47 +0900

    update

--- src/_04_standard_ans/route/web.php
+++ src/_04_standard_ans/route/web.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 use App\Http\Api\HelloWorldAction;
+use App\Http\Middleware\FilterWordMiddleware;
 use Route\Route;
 
 Route::addRoute('GET', '/hello', [
-    'Filter' => [],
+    'Filter' => [new FilterWordMiddleware()],
     'Action' => new HelloWorldAction()
 ]);
